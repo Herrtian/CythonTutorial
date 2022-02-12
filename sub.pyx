592bd9c546102d527465db9cf9a1afbeda6b56ef
herrtian <herrtian@3097079240@qq.com>
2022-02-12 16:11:29 +0800

    sub_with_c

--- sub.pyx
+++ sub.pyx
@@ -1,6 +1,10 @@
+import cython
 cdef extern from "sub_core/sub.h":
     int sub(int a, int b)
 
 # 然后 Cython 可以直接调用
+@cython.infer_types(True)
 def sub_with_c(a, b):
-    return sub(a, b)
+    cdef int ret = sub(a, b)  # 这一行会更快 原因在于静态类型
+    # ret = sub(a, b)
+    return ret
